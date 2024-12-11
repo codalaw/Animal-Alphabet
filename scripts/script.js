b9142ea4d1dddece876bd3302fd1b22e8cb97302
Cody Lawson <codalaw@gmail.com>
2024-12-11 11:05:44 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,294 @@
+
+document.getElementById('aardvarkImage').addEventListener('click', function() {
+    document.getElementById('aardvarkImage').classList.add('hidden');
+    document.getElementById('aardvarkVideo').classList.remove('hidden');
+    document.getElementById('aardvarkVideo').play();
+});
+
+document.getElementById('aardvarkVideo').addEventListener('click', function() {
+    document.getElementById('aardvarkVideo').classList.add('hidden');
+    document.getElementById('aardvarkImage').classList.remove('hidden');
+});
+
+
+document.getElementById('bearImage').addEventListener('click', function() {
+    document.getElementById('bearImage').classList.add('hidden');
+    document.getElementById('bearVideo').classList.remove('hidden');
+    document.getElementById('bearVideo').play();
+});
+
+document.getElementById('bearVideo').addEventListener('click', function() {
+    document.getElementById('bearVideo').classList.add('hidden');
+    document.getElementById('bearImage').classList.remove('hidden');
+});
+
+
+document.getElementById('catImage').addEventListener('click', function() {
+    document.getElementById('catImage').classList.add('hidden');
+    document.getElementById('catVideo').classList.remove('hidden');
+    document.getElementById('catVideo').play();
+});
+
+document.getElementById('catVideo').addEventListener('click', function() {
+    document.getElementById('catVideo').classList.add('hidden');
+    document.getElementById('catImage').classList.remove('hidden');
+});
+
+
+document.getElementById('dogImage').addEventListener('click', function() {
+    document.getElementById('dogImage').classList.add('hidden');
+    document.getElementById('dogVideo').classList.remove('hidden');
+    document.getElementById('dogVideo').play();
+});
+
+document.getElementById('dogVideo').addEventListener('click', function() {
+    document.getElementById('dogVideo').classList.add('hidden');
+    document.getElementById('dogImage').classList.remove('hidden');
+});
+
+
+document.getElementById('elephantImage').addEventListener('click', function() {
+    document.getElementById('elephantImage').classList.add('hidden');
+    document.getElementById('elephantVideo').classList.remove('hidden');
+    document.getElementById('elephantVideo').play();
+});
+
+document.getElementById('elephantVideo').addEventListener('click', function() {
+    document.getElementById('elephantVideo').classList.add('hidden');
+    document.getElementById('elephantImage').classList.remove('hidden');
+});
+
+
+document.getElementById('ferretImage').addEventListener('click', function() {
+    document.getElementById('ferretImage').classList.add('hidden');
+    document.getElementById('ferretVideo').classList.remove('hidden');
+    document.getElementById('ferretVideo').play();
+});
+
+document.getElementById('ferretVideo').addEventListener('click', function() {
+    document.getElementById('ferretVideo').classList.add('hidden');
+    document.getElementById('ferretImage').classList.remove('hidden');
+});
+
+document.getElementById('goatImage').addEventListener('click', function() {
+    document.getElementById('goatImage').classList.add('hidden');
+    document.getElementById('goatVideo').classList.remove('hidden');
+    document.getElementById('goatVideo').play();
+});
+
+document.getElementById('goatVideo').addEventListener('click', function() {
+    document.getElementById('goatVideo').classList.add('hidden');
+    document.getElementById('goatImage').classList.remove('hidden');
+});
+
+
+document.getElementById('horseImage').addEventListener('click', function() {
+    document.getElementById('horseImage').classList.add('hidden');
+    document.getElementById('horseVideo').classList.remove('hidden');
+    document.getElementById('horseVideo').play();
+});
+
+document.getElementById('horseVideo').addEventListener('click', function() {
+    document.getElementById('horseVideo').classList.add('hidden');
+    document.getElementById('horseImage').classList.remove('hidden');
+});
+
+document.getElementById('iguanaImage').addEventListener('click', function() {
+    document.getElementById('iguanaImage').classList.add('hidden');
+    document.getElementById('iguanaVideo').classList.remove('hidden');
+    document.getElementById('iguanaVideo').play();
+});
+
+document.getElementById('iguanaVideo').addEventListener('click', function() {
+    document.getElementById('iguanaVideo').classList.add('hidden');
+    document.getElementById('iguanaImage').classList.remove('hidden');
+});
+
+document.getElementById('jaguarImage').addEventListener('click', function() {
+    document.getElementById('jaguarImage').classList.add('hidden');
+    document.getElementById('jaguarVideo').classList.remove('hidden');
+    document.getElementById('jaguarVideo').play();
+});
+
+document.getElementById('jaguarVideo').addEventListener('click', function() {
+    document.getElementById('jaguarVideo').classList.add('hidden');
+    document.getElementById('jaguarImage').classList.remove('hidden');
+});
+
+document.getElementById('kangarooImage').addEventListener('click', function() {
+    document.getElementById('kangarooImage').classList.add('hidden');
+    document.getElementById('kangarooVideo').classList.remove('hidden');
+    document.getElementById('kangarooVideo').play();
+});
+
+document.getElementById('kangarooVideo').addEventListener('click', function() {
+    document.getElementById('kangarooVideo').classList.add('hidden');
+    document.getElementById('kangarooImage').classList.remove('hidden');
+});
+
+document.getElementById('llamaImage').addEventListener('click', function() {
+    document.getElementById('llamaImage').classList.add('hidden');
+    document.getElementById('llamaVideo').classList.remove('hidden');
+    document.getElementById('llamaVideo').play();
+});
+
+document.getElementById('llamaVideo').addEventListener('click', function() {
+    document.getElementById('llamaVideo').classList.add('hidden');
+    document.getElementById('llamaImage').classList.remove('hidden');
+});
+
+document.getElementById('monkeyImage').addEventListener('click', function() {
+    document.getElementById('monkeyImage').classList.add('hidden');
+    document.getElementById('monkeyVideo').classList.remove('hidden');
+    document.getElementById('monkeyVideo').play();
+});
+
+document.getElementById('monkeyVideo').addEventListener('click', function() {
+    document.getElementById('monkeyVideo').classList.add('hidden');
+    document.getElementById('monkeyImage').classList.remove('hidden');
+});
+
+
+document.getElementById('narwalImage').addEventListener('click', function() {
+    document.getElementById('narwalImage').classList.add('hidden');
+    document.getElementById('narwalVideo').classList.remove('hidden');
+    document.getElementById('narwalVideo').play();
+});
+
+document.getElementById('narwalVideo').addEventListener('click', function() {
+    document.getElementById('narwalVideo').classList.add('hidden');
+    document.getElementById('narwalImage').classList.remove('hidden');
+});
+
+document.getElementById('ostrichImage').addEventListener('click', function() {
+    document.getElementById('ostrichImage').classList.add('hidden');
+    document.getElementById('ostrichVideo').classList.remove('hidden');
+    document.getElementById('ostrichVideo').play();
+});
+
+document.getElementById('ostrichVideo').addEventListener('click', function() {
+    document.getElementById('ostrichVideo').classList.add('hidden');
+    document.getElementById('ostrichImage').classList.remove('hidden');
+});
+
+document.getElementById('pandaImage').addEventListener('click', function() {
+    document.getElementById('pandaImage').classList.add('hidden');
+    document.getElementById('pandaVideo').classList.remove('hidden');
+    document.getElementById('pandaVideo').play();
+});
+
+document.getElementById('pandaVideo').addEventListener('click', function() {
+    document.getElementById('pandaVideo').classList.add('hidden');
+    document.getElementById('pandaImage').classList.remove('hidden');
+});
+
+document.getElementById('quailImage').addEventListener('click', function() {
+    document.getElementById('quailImage').classList.add('hidden');
+    document.getElementById('quailVideo').classList.remove('hidden');
+    document.getElementById('quailVideo').play();
+});
+
+document.getElementById('quailVideo').addEventListener('click', function() {
+    document.getElementById('quailVideo').classList.add('hidden');
+    document.getElementById('quailImage').classList.remove('hidden');
+});
+
+
+document.getElementById('raccoonImage').addEventListener('click', function() {
+    document.getElementById('raccoonImage').classList.add('hidden');
+    document.getElementById('raccoonVideo').classList.remove('hidden');
+    document.getElementById('raccoonVideo').play();
+});
+
+document.getElementById('raccoonVideo').addEventListener('click', function() {
+    document.getElementById('raccoonVideo').classList.add('hidden');
+    document.getElementById('raccoonImage').classList.remove('hidden');
+});
+
+document.getElementById('skunkImage').addEventListener('click', function() {
+    document.getElementById('skunkImage').classList.add('hidden');
+    document.getElementById('skunkVideo').classList.remove('hidden');
+    document.getElementById('skunkVideo').play();
+});
+
+document.getElementById('skunkVideo').addEventListener('click', function() {
+    document.getElementById('skunkVideo').classList.add('hidden');
+    document.getElementById('skunkImage').classList.remove('hidden');
+});
+
+document.getElementById('turkeyImage').addEventListener('click', function() {
+    document.getElementById('turkeyImage').classList.add('hidden');
+    document.getElementById('turkeyVideo').classList.remove('hidden');
+    document.getElementById('turkeyVideo').play();
+});
+
+document.getElementById('turkeyVideo').addEventListener('click', function() {
+    document.getElementById('turkeyVideo').classList.add('hidden');
+    document.getElementById('turkeyImage').classList.remove('hidden');
+});
+
+document.getElementById('uakariImage').addEventListener('click', function() {
+    document.getElementById('uakariImage').classList.add('hidden');
+    document.getElementById('uakariVideo').classList.remove('hidden');
+    document.getElementById('uakariVideo').play();
+});
+
+document.getElementById('uakariVideo').addEventListener('click', function() {
+    document.getElementById('uakariVideo').classList.add('hidden');
+    document.getElementById('uakariImage').classList.remove('hidden');
+});
+
+document.getElementById('voleImage').addEventListener('click', function() {
+    document.getElementById('voleImage').classList.add('hidden');
+    document.getElementById('voleVideo').classList.remove('hidden');
+    document.getElementById('voleVideo').play();
+});
+
+document.getElementById('voleVideo').addEventListener('click', function() {
+    document.getElementById('voleVideo').classList.add('hidden');
+    document.getElementById('voleImage').classList.remove('hidden');
+});
+
+document.getElementById('walrusImage').addEventListener('click', function() {
+    document.getElementById('walrusImage').classList.add('hidden');
+    document.getElementById('walrusVideo').classList.remove('hidden');
+    document.getElementById('walrusVideo').play();
+});
+
+document.getElementById('walrusVideo').addEventListener('click', function() {
+    document.getElementById('walrusVideo').classList.add('hidden');
+    document.getElementById('walrusImage').classList.remove('hidden');
+});
+
+document.getElementById('xerusImage').addEventListener('click', function() {
+    document.getElementById('xerusImage').classList.add('hidden');
+    document.getElementById('xerusVideo').classList.remove('hidden');
+    document.getElementById('xerusVideo').play();
+});
+
+document.getElementById('xerusVideo').addEventListener('click', function() {
+    document.getElementById('xerusVideo').classList.add('hidden');
+    document.getElementById('xerusImage').classList.remove('hidden');
+});
+
+document.getElementById('yakImage').addEventListener('click', function() {
+    document.getElementById('yakImage').classList.add('hidden');
+    document.getElementById('yakVideo').classList.remove('hidden');
+    document.getElementById('yakVideo').play();
+});
+
+document.getElementById('yakVideo').addEventListener('click', function() {
+    document.getElementById('yakVideo').classList.add('hidden');
+    document.getElementById('yakImage').classList.remove('hidden');
+});
+
+document.getElementById('zebraImage').addEventListener('click', function() {
+    document.getElementById('zebraImage').classList.add('hidden');
+    document.getElementById('zebraVideo').classList.remove('hidden');
+    document.getElementById('zebraVideo').play();
+});
+
+document.getElementById('zebraVideo').addEventListener('click', function() {
+    document.getElementById('zebraVideo').classList.add('hidden');
+    document.getElementById('zebraImage').classList.remove('hidden');
+});
